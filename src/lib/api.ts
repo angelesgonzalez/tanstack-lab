56c08bf445975ba304575a72b82a6fa4f50a1ae0
Angeles <delosangelesgonzalez@gmail.com>
2026-08-28 17:42:36 +0200

    Add House types and  API client with server functions

---
+++ src/lib/api.ts
@@ -0,0 +1,34 @@
+import { createServerFn } from "@tanstack/react-start";
+import { House } from "./types";
+
+export const getHouses = createServerFN({ method: 'GET' }).handler(
+
+    async (): Promise<House[]> => {
+
+        const res = await fetch(`${process.env.API_URL}/api/houses`);
+
+        if (!res.ok) {
+            throw new Error(`There's been an error while fetching the houses, check the api.`)
+        }
+
+        return res.json();
+    },
+
+);
+
+export const getHouseById = createServerFn({ method: "GET" })
+    .validator((id: string) => id)
+    .handler(async ({ data: id }): Promise<House | undefined> => {
+        const res = await fetch(`${process.env.API_URL}/api/houses/${id}`);
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch house");
+        }
+
+        const text = await res.text();
+        if (!text) {
+            return undefined;
+        }
+
+        return JSON.parse(text);
+    });
