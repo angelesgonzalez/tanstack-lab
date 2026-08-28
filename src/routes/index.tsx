4e5e4325006eae603aae63cbf9569f388ba5f591
Angeles <delosangelesgonzalez@gmail.com>
2026-08-28 17:58:08 +0200

    Edit index.tsx to call houses & vm to populate the list

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,30 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { getHouses } from "@/lib/api";
+import { toHouseCardVM } from "#/lib/mappers";
+import { HouseCard } from "@/components/HouseCard";
 
-export const Route = createFileRoute('/')({ component: Home })
+
+export const Route = createFileRoute("/")({
+  loader: async () => {
+    const houses = await getHouses();
+    return houses.map(toHouseCardVM);
+  },
+  component: Home,
+});
 
 function Home() {
+  const houseCards = Route.useLoaderData();
+
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
+    <div>
+      <h1 className="font-mono text-xs uppercase tracking-widest text-zinc-500">
+        {houseCards.length} casas rurales
+      </h1>
+      <div className="mt-8 grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
+        {houseCards.map((house) => (
+          <HouseCard key={house.id} house={house} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
